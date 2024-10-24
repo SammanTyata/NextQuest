4e4d96889a1203004e325839a4f50d53f45b879a
Samman <tyatasamman@gmail.com>
2024-10-24 15:19:51 -0700

    Initial Commit

---
+++ NextQuest/NextQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextQuestApp.swift
+//  NextQuest
+//
+//  Created by Samman Tyata on 10/24/24.
+//
+
+import SwiftUI
+
+@main
+struct NextQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
